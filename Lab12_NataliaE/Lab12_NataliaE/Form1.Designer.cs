090251a912bf73e2576d1876f263eab4a4a33022
NataliaEspinola <naespinola@miuandes.cl>
2018-10-25 16:22:27 -0300

    Vista

---
+++ Lab12_NataliaE/Lab12_NataliaE/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Lab12_NataliaE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.AgregarButton = new System.Windows.Forms.Button();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.Carbohidratos = new System.Windows.Forms.Label();
+            this.Grasas = new System.Windows.Forms.Label();
+            this.CarbohidratosTextBox = new System.Windows.Forms.TextBox();
+            this.proteinasTextBox = new System.Windows.Forms.TextBox();
+            this.grasasTextBox = new System.Windows.Forms.TextBox();
+            this.Proteinas = new System.Windows.Forms.Label();
+            this.listado = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.eliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(29, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Nombre);
+            this.splitContainer1.Panel1.Controls.Add(this.AgregarButton);
+            this.splitContainer1.Panel1.Controls.Add(this.nombreTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.Carbohidratos);
+            this.splitContainer1.Panel1.Controls.Add(this.Grasas);
+            this.splitContainer1.Panel1.Controls.Add(this.CarbohidratosTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.proteinasTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.grasasTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.Proteinas);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.eliminar);
+            this.splitContainer1.Panel2.Controls.Add(this.listado);
+            this.splitContainer1.Panel2.Controls.Add(this.listBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(700, 426);
+            this.splitContainer1.SplitterDistance = 292;
+            this.splitContainer1.TabIndex = 10;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(43, 61);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(44, 13);
+            this.Nombre.TabIndex = 0;
+            this.Nombre.Text = "Nombre";
+            // 
+            // AgregarButton
+            // 
+            this.AgregarButton.Location = new System.Drawing.Point(104, 325);
+            this.AgregarButton.Name = "AgregarButton";
+            this.AgregarButton.Size = new System.Drawing.Size(75, 23);
+            this.AgregarButton.TabIndex = 8;
+            this.AgregarButton.Text = "Agregar";
+            this.AgregarButton.UseVisualStyleBackColor = true;
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.Location = new System.Drawing.Point(151, 58);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nombreTextBox.TabIndex = 4;
+            // 
+            // Carbohidratos
+            // 
+            this.Carbohidratos.AutoSize = true;
+            this.Carbohidratos.Location = new System.Drawing.Point(43, 255);
+            this.Carbohidratos.Name = "Carbohidratos";
+            this.Carbohidratos.Size = new System.Drawing.Size(72, 13);
+            this.Carbohidratos.TabIndex = 2;
+            this.Carbohidratos.Text = "Carbohidratos";
+            // 
+            // Grasas
+            // 
+            this.Grasas.AutoSize = true;
+            this.Grasas.Location = new System.Drawing.Point(43, 186);
+            this.Grasas.Name = "Grasas";
+            this.Grasas.Size = new System.Drawing.Size(40, 13);
+            this.Grasas.TabIndex = 3;
+            this.Grasas.Text = "Grasas";
+            // 
+            // CarbohidratosTextBox
+            // 
+            this.CarbohidratosTextBox.Location = new System.Drawing.Point(151, 252);
+            this.CarbohidratosTextBox.Name = "CarbohidratosTextBox";
+            this.CarbohidratosTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CarbohidratosTextBox.TabIndex = 7;
+            // 
+            // proteinasTextBox
+            // 
+            this.proteinasTextBox.Location = new System.Drawing.Point(151, 118);
+            this.proteinasTextBox.Name = "proteinasTextBox";
+            this.proteinasTextBox.Size = new System.Drawing.Size(100, 20);
+            this.proteinasTextBox.TabIndex = 5;
+            // 
+            // grasasTextBox
+            // 
+            this.grasasTextBox.Location = new System.Drawing.Point(151, 183);
+            this.grasasTextBox.Name = "grasasTextBox";
+            this.grasasTextBox.Size = new System.Drawing.Size(100, 20);
+            this.grasasTextBox.TabIndex = 6;
+            // 
+            // Proteinas
+            // 
+            this.Proteinas.AutoSize = true;
+            this.Proteinas.Location = new System.Drawing.Point(43, 121);
+            this.Proteinas.Name = "Proteinas";
+            this.Proteinas.Size = new System.Drawing.Size(51, 13);
+            this.Proteinas.TabIndex = 1;
+            this.Proteinas.Text = "Proteinas";
+            // 
+            // listado
+            // 
+            this.listado.AutoSize = true;
+            this.listado.Location = new System.Drawing.Point(179, 42);
+            this.listado.Name = "listado";
+            this.listado.Size = new System.Drawing.Size(41, 13);
+            this.listado.TabIndex = 1;
+            this.listado.Text = "Listado";
+            this.listado.Click += new System.EventHandler(this.listado_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(65, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // eliminar
+            // 
+            this.eliminar.AutoSize = true;
+            this.eliminar.Location = new System.Drawing.Point(152, 325);
+            this.eliminar.Name = "eliminar";
+            this.eliminar.Size = new System.Drawing.Size(97, 23);
+            this.eliminar.TabIndex = 2;
+            this.eliminar.Text = "Eliminar Eleccion";
+            this.eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 454);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Button AgregarButton;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.Label Carbohidratos;
+        private System.Windows.Forms.Label Grasas;
+        private System.Windows.Forms.TextBox CarbohidratosTextBox;
+        private System.Windows.Forms.TextBox proteinasTextBox;
+        private System.Windows.Forms.TextBox grasasTextBox;
+        private System.Windows.Forms.Label Proteinas;
+        private System.Windows.Forms.Label listado;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button eliminar;
+    }
+}
+
